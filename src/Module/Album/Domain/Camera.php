8ba6b56c6ab32758ee9345859e53178b35aa6b3c
Tom Wilford <hello@jollyblueman.com>
2026-05-30 11:13:46 +0100

    * Added cameras

--- src/Module/Album/Domain/Camera.php
+++ src/Module/Album/Domain/Camera.php
@@ -9,11 +9,19 @@ enum Camera: string
     case YASHICA_635 = 'yashica635';
     case OLYMPUS_PEN = 'olympusPen';
     case OLYMPUS_35RC = 'olympus35RC';
+    case CHAIKA_2 = 'chaika2';
+    case PENTAX_LX = 'pentaxLX';
+    case PENTAX_ME_SUPER = 'pentaxMESuper';
+    case ZENIT_E = 'zenitE';
+    case RICOH_KR10_SUPER = 'ricohKr10Super';
 
     public function getDisplayName(): string
     {
         return match ($this) {
             self::OLYMPUS_35RC => 'Olympus 35RC',
+            self::PENTAX_LX => 'Pentax LX',
+            self::PENTAX_ME_SUPER => 'Pentax ME Super',
+            self::RICOH_KR10_SUPER => 'Ricoh KR10 Super',
             default => ucwords(strtolower(str_replace('_', ' ', $this->name))),
         };
     }
